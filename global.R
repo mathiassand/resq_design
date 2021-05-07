6d7028b5b418427c710075af0221eda3f0555614
mathiassand <mskr16@student.aau.dk>
2021-05-07 15:34:42 +0200

    re-design prototype

--- global.R
+++ global.R
@@ -16,78 +16,12 @@ resq_anon <- read_csv("anon_data_Hendrik.csv")
 
 resq_clean <- na.omit(resq_anon)
 
-resq_median <- resq_clean %>%
-  group_by(country, hospital, year) %>%
-  summarise(DTNMedian = median(DTN)) %>%
-  ungroup()
-
-resq_df <- resq_clean %>%
-  # filtering for years after 2015 only, as Mirek told us RES-Q was added in 2016.
-  # Dataset has a lot of misstypes, or errors? 1900, 2013, 2014 etc.
-  filter(year > 2015) %>%
-  group_by(hospital) %>%
-  summarise(
-    firstYear = min(year),
-    latestYear = max(year),
-    noOfYears = n_distinct(year)
-  ) %>%
-  ungroup()
-
-
-
-resq_count <- resq_clean %>%
-  group_by(country, year, DTN) %>%
-  summarise(DTNCount = n()) %>%
-  ungroup()
-
-join_df <- merge(resq_df, resq_median, by = "hospital") %>% mutate(runningYear = year - firstYear + 1)
-
-country_median_df <- join_df %>%
-  group_by(country, runningYear) %>%
-  filter(country == "Country #54", runningYear >= 1, runningYear <= 3) %>%
-  summarise(countryMedian = median(DTNMedian)) %>%
-  ungroup()
-
-hospital_median_df <- join_df %>%
-  group_by(hospital, runningYear) %>%
-  filter(hospital == "Hospital #106", runningYear >= 1, runningYear <= 3) %>%
-  summarise(hospitalMedian = mean(DTNMedian)) %>%
-  ungroup()
-
-ch_median_df <- merge(country_median_df, hospital_median_df, by = "runningYear")
-
-# VISUAL 2 CUTS, FIRST THREE CUTS ARE BASED ON ANGELS INITITATIVE CUTS, DIVIDED 60-120 FURTHER
-DTN_breaks <- c(-Inf, 30, 45, 60, 80, 90, 100, 120, Inf)
-DTN_break_labels <- c("<30", "30-45", "45-60", "60-80", "80-90", "90-100", "100-120", ">120")
-
-cohort_df <- join_df %>%
-  ungroup() %>%
-  group_by(runningYear) %>%
-  mutate(
-    DTNMedianCohort = cut(DTNMedian, breaks = DTN_breaks, labels = DTN_break_labels)
-  ) %>%
-  ungroup()
-
-
-cohort_df <- cohort_df %>%
-  filter(runningYear == 2) %>%
-  select(hospital, DTNMedianCohort) %>%
-  rename(startingDTNMedianCohort = DTNMedianCohort) %>%
-  merge(cohort_df)
-
-cohort_filter_df <- cohort_df %>%
-  filter(startingDTNMedianCohort == "30-45" & year > 2016 & runningYear <= 6) %>%
-  group_by(runningYear) %>%
-  summarise(
-    cohortMeanofMedian = mean(DTNMedian)
-  )
-
-
+#Current Status Dashboard variables
 resq_106_CS_df <- resq_clean %>%
   # filtering for years after 2015 only, as Mirek told us RES-Q was added in 2016.
   # Dataset has a lot of misstypes, or errors? 1900, 2013, 2014 etc.
   # filtering for DTN less than 10 and larger than 400, ask Mirek told us these are "normal" cutting points
-  filter(year > 2015, year <= 2020, DTN > 10, DTN < 400, hospital == "Hospital #106", `stroke type` == "ischemic") %>%
+  filter(year >= 2016, year <= 2020, DTN > 10, DTN < 400, hospital == "Hospital #196", `stroke type` == "ischemic") %>%
   group_by(hospital, year) %>%
   summarise(
     firstYear = min(year),
@@ -100,3 +34,69 @@ resq_106_CS_df <- resq_clean %>%
     patient_count = n_distinct(subject_id)
   ) %>%
   ungroup()
+
+resq_median <- resq_clean %>%
+  group_by(country, hospital, year) %>%
+  filter(DTN < 400, DTN > 10, year >= 2016) %>%
+  summarise(DTNMedian = median(DTN)) %>%
+  ungroup()
+
+resq_df <- resq_clean %>%
+  # filtering for years after 2015 only, as Mirek told us RES-Q was added in 2016.
+  # Dataset has a lot of misstypes, or errors? 1900, 2013, 2014 etc.
+  filter(year >= 2016) %>%
+  group_by(hospital) %>%
+  summarise(
+    firstYear = min(year),
+    latestYear = max(year),
+    noOfYears = n_distinct(year)
+  ) %>%
+  ungroup()
+
+join_df <- merge(resq_df, resq_median, by = "hospital") %>% mutate(runningYear = year - firstYear + 1)
+
+
+# VISUAL 2 CUTS, FIRST THREE CUTS ARE BASED ON ANGELS INITITATIVE CUTS, DIVIDED 60-120 FURTHER
+DTN_breaks <- c(-Inf, 30, 45, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, Inf)
+DTN_break_labels <- c("<30", "30-45", "45-60", "60-80", "80-100", "100-120", "120-140", 
+                      "140-160", "160-180", "180-200", "200-220", "220-240", ">240")
+
+cohort_df <- join_df %>%
+  ungroup() %>%
+  group_by(runningYear) %>%
+  mutate(
+    DTNMedianCohort = cut(DTNMedian, breaks = DTN_breaks, labels = DTN_break_labels)
+  ) %>%
+  ungroup()
+
+country_median_df <- join_df %>%
+  group_by(country, year) %>%
+  filter(country == "Country #53") %>%
+  summarise(countryMedian = median(DTNMedian)) %>%
+  ungroup()
+
+hospital_median_df <- join_df %>%
+  group_by(hospital, year) %>%
+  filter(hospital == "Hospital #338") %>%
+  summarise(hospitalMedian = median(DTNMedian)) %>%
+  ungroup()
+
+ch_median_df <- merge(country_median_df, hospital_median_df, by = "year")
+
+# if(hospital_median_df$runningYear == 2){
+#   
+# }
+
+cohort_df <- cohort_df %>%
+  filter(year == 2018) %>%
+  #filter(hospital_median_df$runningYear == 1) %>%
+  select(hospital, DTNMedianCohort) %>%
+  rename(startingDTNMedianCohort = DTNMedianCohort) %>%
+  merge(cohort_df)
+
+cohort_filter_df <- cohort_df %>%
+  filter(startingDTNMedianCohort == "80-100", year == hospital_median_df$year) %>%
+  group_by(year) %>%
+  summarise(
+    cohortMeanofMedian = mean(DTNMedian)
+  )
--- server.R
+++ server.R
@@ -3,16 +3,15 @@ server <- shinyServer(function(input, output) {
   output$visual1 <- renderPlot(
     ch_median_df %>%
       ungroup() %>%
-      ggplot(aes(x = runningYear, y = hospitalMedian)) +
-      geom_smooth(se = FALSE, alpha = .5, color = "purple") +
+      ggplot(aes(x = year, y = hospitalMedian)) +
+      #geom_smooth(se = FALSE, alpha = .5, color = "purple") +
+      geom_line(color="purple", size = 2) +
       geom_point(shape = 21, color = "purple", fill = "white", size = 5, stroke = 1.7) +
-      geom_smooth(aes(y = countryMedian), se = FALSE, alpha = .5, color = "red") +
-      geom_point(aes(y = countryMedian), shape = 21, color = "red", fill = "white", size = 5, stroke = 1.7) +
       theme_classic() +
       coord_cartesian(ylim = c(0, 150)) +
-      scale_x_continuous(breaks = seq(1, 6, 1)) +
+      scale_x_continuous(breaks = ch_median_df$year) +
       labs(
-        title = "<span style = 'color: purple;'>Your hospital</span>compared to the <span style = 'color: red;'>national median</span>",
+        title = "<span style = 'color: purple;'>Your hospital</span>",
         y = "Median DTN", x = "Years in RES-Q"
       ) +
       theme(
@@ -25,23 +24,20 @@ server <- shinyServer(function(input, output) {
   output$visual2 <- renderPlot(
     ch_median_df %>%
       ungroup() %>%
-      ggplot(aes(x = runningYear, y = hospitalMedian)) +
-      geom_smooth(se = FALSE, alpha = .5, color = "purple") +
+      ggplot(aes(x = year, y = hospitalMedian)) +
+      geom_line(color = "purple", size = 2) +
       geom_point(shape = 21, color = "purple", fill = "white", size = 5, stroke = 1.7) +
-      geom_smooth(aes(y = countryMedian), se = FALSE, alpha = .5, color = "red") +
+      geom_line(aes(y = countryMedian), color = "red", size = 2) +
       geom_point(aes(y = countryMedian), shape = 21, color = "red", fill = "white", size = 5, stroke = 1.7) +
-      geom_smooth(data = cohort_filter_df, aes(x = runningYear, y = cohortMeanofMedian), se = FALSE, alpha = .5, color = "#56B4E9") +
-      geom_point(data = cohort_filter_df, aes(x = runningYear, y = cohortMeanofMedian), shape = 21, color = "#56B4E9", fill = "white", size = 5, stroke = 1.7) +
       theme_classic() +
       coord_cartesian(ylim = c(0, 150)) +
-      scale_x_continuous(breaks = seq(1, 6, 1)) +
+      scale_x_continuous(breaks = ch_median_df$year) +
       labs(
-        title = "<span style = 'color: purple;'>Your hospital</span> compared to the <span style = 'color: red;'>national median</span>
-        and a <span style = 'color: #56B4E9;'>group of hospitals</span> similar to your first three years in RES-Q",
+        title = "<span style = 'color: purple;'>Your hospital</span>compared to the <span style = 'color: red;'>national median</span>",
         y = "Median DTN", x = "Years in RES-Q"
       ) +
       theme(
-        plot.title = element_markdown(size = 15),
+        plot.title = element_markdown(size = 20),
         axis.title.x = element_text(hjust = .05, vjust = 0.2),
         axis.title.y = element_text(hjust = 0.95, vjust = 0.9),
       )
@@ -50,28 +46,26 @@ server <- shinyServer(function(input, output) {
   output$visual3 <- renderPlot(
     ch_median_df %>%
       ungroup() %>%
-      ggplot(aes(x = runningYear, y = hospitalMedian)) +
-      geom_smooth(se = FALSE, alpha = .5, color = "purple") +
+      ggplot(aes(x = year, y = hospitalMedian)) +
+      geom_line(color = "purple", size = 2) +
       geom_point(shape = 21, color = "purple", fill = "white", size = 5, stroke = 1.7) +
-      geom_smooth(aes(y = countryMedian), se = FALSE, alpha = .5, color = "red") +
+      geom_line(aes(y = countryMedian), color = "red", size = 2) +
       geom_point(aes(y = countryMedian), shape = 21, color = "red", fill = "white", size = 5, stroke = 1.7) +
-      geom_smooth(data = cohort_filter_df, aes(x = runningYear, y = cohortMeanofMedian), se = FALSE, alpha = .5, color = "#56B4E9") +
-      geom_point(data = cohort_filter_df, aes(x = runningYear, y = cohortMeanofMedian), shape = 21, color = "#56B4E9", fill = "white", size = 5, stroke = 1.7) +
+      geom_line(data = cohort_filter_df, aes(x = year, y = cohortMeanofMedian), color = "#56B4E9", size = 2) +
+      geom_point(data = cohort_filter_df, aes(x = year, y = cohortMeanofMedian), shape = 21, color = "#56B4E9", fill = "white", size = 5, stroke = 1.7) +
       theme_classic() +
       coord_cartesian(ylim = c(0, 150)) +
-      scale_x_continuous(breaks = seq(1, 6, 1)) +
+      scale_x_continuous(breaks = ch_median_df$year) +
       labs(
         title = "<span style = 'color: purple;'>Your hospital</span> compared to the <span style = 'color: red;'>national median</span>
-        and a <span style = 'color: #56B4E9;'>group of hospitals</span> similar to your first three years in RES-Q",
+        and a <span style = 'color: #56B4E9;'>group of hospitals</span> similar to your first two years in RES-Q",
         y = "Median DTN", x = "Years in RES-Q"
       ) +
       theme(
         plot.title = element_markdown(size = 15),
         axis.title.x = element_text(hjust = .05, vjust = 0.2),
         axis.title.y = element_text(hjust = 0.95, vjust = 0.9)
-      ) +
-      annotate("rect", xmin = 3.1, xmax = 6.1, ymin = 0, ymax = 150, alpha = 0.2, color = "grey", fill = "grey") +
-      annotate("text", size = 6, x = 4.5, y = 10, label = "Possible future")
+      )
   )
 
   # output$visual4 <- renderPlotly(
